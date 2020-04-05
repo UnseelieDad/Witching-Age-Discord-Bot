2611a15089438d15142604ba66f90038224f9eef
UnseelieDad <voiceofsethmartin@gmail.com>
2020-04-04 22:27:31 -0500

    changed from d6 to hack and slash

--- balasar.py
+++ balasar.py
@@ -16,9 +16,23 @@ bot = commands.Bot(command_prefix="!")
 async def on_ready():
     print(f"{bot.user.name} has connected to Discord!")
 
-@bot.command(name="d6", help="Rolls a d6")
-async def d6(ctx):
-    response = random.choice(range(1,7))
+@bot.command(name="hs", help="Rolls the hack and slash move from dungeon world")
+async def hack_and_slash(ctx):
+    
+    d1 = random.randint(1,6)
+    d2 = random.randint(1,6)
+
+    roll = d1 + d2
+
+    result = f"{d1} + {d2} = {roll}:"
+
+    if roll >= 10:
+        response = f"{result}\n Deal your damage and choose one:\n - Avoid the enemy's attack\n - Deal an extra d6 damage"
+    elif roll <= 9 and roll >= 7:
+        response = f"{result}\n Deal your damage and the enemy makes an attack against you."
+    else:
+        response = f"{result}\n Miss. Mark XP"
+    
     await ctx.send(response)
 
 bot.run(TOKEN)
