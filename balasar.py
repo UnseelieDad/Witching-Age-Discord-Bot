dd0da131df85efa8a3a59a7e40b6762aa5945571
UnseelieDad <voiceofsethmartin@gmail.com>
2020-04-14 19:49:21 -0500

    Added roll dice function

--- balasar.py
+++ balasar.py
@@ -35,4 +35,36 @@ async def hack_and_slash(ctx):
     
     await ctx.send(response)
 
+@bot.command(name="roll", aliases=["r"], help="Rolls specified dice and modifers (xdy+z)")
+async def roll_dice(ctx, roll_string : str):
+    
+    roll_string.replace(" ", "")
+    dice_number = int(roll_string[0])
+    dice_type = int(roll_string[2])
+
+    dice = []
+    for die in range(0,dice_number):
+        die = random.randint(1, dice_type)
+        dice.append(die)
+    
+    total = sum(dice)
+    
+    dice = list(map(str, dice))
+    response = "| + |".join(dice)
+    response = "|" + response + "|"
+
+    
+    if len(roll_string) > 3:
+        mod_sign = roll_string[3]
+        modifier = int(roll_string[4])
+        if mod_sign == "-":
+            total -= modifier
+        elif mod_sign == '+':
+            total += modifier
+        response += f" + {modifier}"
+
+    response += f"\n= {total}"
+
+    await ctx.send(response)
+
 bot.run(TOKEN)
